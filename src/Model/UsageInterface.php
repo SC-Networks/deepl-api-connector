628e76c7edf98824dc94b19110fc6a0a9e4594c3
Sascha Nützel <nuetzel@sc-networks.com>
2018-06-22 11:27:21 +0200

    Add missing Interface Methods

--- src/Model/UsageInterface.php
+++ src/Model/UsageInterface.php
@@ -10,4 +10,27 @@ namespace Scn\DeeplApiConnector\Model;
 interface UsageInterface
 {
 
+    /**
+     * @return int
+     */
+    public function getCharacterCount();
+
+    /**
+     * @param int $characterCount
+     *
+     * @return Usage
+     */
+    public function setCharacterCount($characterCount);
+
+    /**
+     * @return int
+     */
+    public function getCharacterLimit();
+
+    /**
+     * @param int $characterLimit
+     *
+     * @return Usage
+     */
+    public function setCharacterLimit($characterLimit);
 }
