affcfb45f2a5703b78f4ab8014a7a8468a01ced2
Sascha Nützel <nuetzel@sc-networks.com>
2020-07-16 14:59:31 +0200

    Add new Deepl Languages
    - fixes #27

--- src/Enum/LanguageEnum.php
+++ src/Enum/LanguageEnum.php
@@ -15,4 +15,6 @@ final class LanguageEnum
     public const LANGUAGE_FR = 'FR';
     public const LANGUAGE_PL = 'PL';
     public const LANGUAGE_EN = 'EN';
+    public const LANGUAGE_JA = 'JA';
+    public const LANGUAGE_ZH = 'ZH';
 }
