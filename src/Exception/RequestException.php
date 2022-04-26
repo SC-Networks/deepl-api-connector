a0645824c6804bb67af49ab8faa0ea0af9bee4c5
Mistralys <s.mordziol@gmail.com>
2022-04-26 17:28:22 +0200

    Added some usage documentation.

--- src/Exception/RequestException.php
+++ src/Exception/RequestException.php
@@ -3,7 +3,17 @@
 namespace Scn\DeeplApiConnector\Exception;
 
 use Exception;
+use Psr\Http\Client\ClientExceptionInterface;
 
+/**
+ * NOTE: Use `getPrevious()` to access the initial
+ * {@see ClientExceptionInterface} exception that was
+ * triggered.
+ *
+ * Depending on the HTTP implementation (e.g. GuzzleHttp),
+ * this can allow access to additional information
+ * (HTTP response details, for example).
+ */
 class RequestException extends Exception
 {
 }
