2127beb1ddffc0a6bbced10e4ca24e8816f48b5e
Sascha Nützel <WorksDev@users.noreply.github.com>
2022-10-14 15:22:26 +0200

    Update Dependencies & Apply some variable descriptions (#67)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,17 +1,11 @@
 <?php
 
-use PhpCsFixer\Config;
-use PhpCsFixer\Finder;
-
-$finder = Finder::create()
-    ->in([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
-
-return (new Config())
-    ->setRules([
-        '@PSR2' => true,
-        'no_unused_imports' => true,
-    ])
-    ->setFinder($finder);
+$config = new PhpCsFixer\Config();
+$config->getFinder()
+    ->exclude('vendor')
+    ->in(__DIR__);
+$config->setRules([
+    '@PSR2' => true,
+    'no_unused_imports' => true,
+]);
+return $config;
